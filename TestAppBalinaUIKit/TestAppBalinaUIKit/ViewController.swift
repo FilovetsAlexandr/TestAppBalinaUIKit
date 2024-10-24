63f7b77b336db1ad094741634de20a5cc8a7bedd
acidsasha <sohan.boxan@gmail.com>
2024-10-24 17:18:05 +0300

    inital commit

---
+++ TestAppBalinaUIKit/TestAppBalinaUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppBalinaUIKit
+//
+//  Created by Alexandr Filovets on 24.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
